563859a2bdfdee1f9948b67a8410e9615cf51504
CameronK1 <62920615+CameronK1@users.noreply.github.com>
2020-04-29 18:24:32 -0700

    Update main.cpp

--- Menu/main.cpp
+++ Menu/main.cpp
@@ -1,6 +1,6 @@
-//#include <iostream>
-//#include "TextureManager.h"
-//#include "ResourceManager.h"
+#include <iostream>
+#include "TextureManager.h"
+#include "ResourceManager.h"
 #include "game.h"
 #include "MenuState.h"
 
@@ -15,41 +15,35 @@ int main()
     game.gameLoop();
 
 
+    TextureManager txtman;
+    txtman.AddResource("Background");
+
+
+    sf::Sprite sprite;
+
+    sprite.setTexture(*(txtman.GetResource("Background")));
 
 
 
+    sf::RenderWindow window(sf::VideoMode(1080, 720), "SFML works!");
 
 
-
-    //TextureManager txtman;
-    //txtman.AddResource("Rock");
-
-
-    //sf::Sprite sprite;
-
-    //sprite.setTexture(*(txtman.GetResource("Rock")));
-
-
-
-    //sf::RenderWindow window(sf::VideoMode(1080, 720), "SFML works!");
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
 
-    //while (window.isOpen())
-    //{
-    //    sf::Event event;
-    //    while (window.pollEvent(event))
-    //    {
-    //        if (event.type == sf::Event::Closed)
-    //            window.close();
-    //    }
+        window.clear();
+        window.draw(sprite);
+        window.display();
+    }
 
-
-    //    window.clear();
-    //    window.draw(sprite);
-    //    window.display();
-    //}
-
-    //txtman.ReleaseResource("Rock");
+    txtman.ReleaseResource("Background");
 
 
     return 0;
